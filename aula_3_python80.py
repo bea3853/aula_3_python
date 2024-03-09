dcf413b4cdc593feb60eefb6f85ab206faf414dd
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2024-03-09 17:47:34 -0300

    Add files via upload

---
+++ aula_3_python80.py
@@ -0,0 +1,23 @@
+#variável  do tipo real -> float
+#real = 1.5
+#print(real)
+
+#variável do tipo texto
+#texto = 'Isso é um texto'
+#texto = 'Continua sendo'
+
+#variável do tipo boleano #1 V #0 F
+#booleano1 = True
+#booleano2 = False
+
+#variável do tipo inteiro
+#numero_inteiro = 10
+
+#concatenação  - >  juntar as informações de output
+nome  =  'Fernando'
+idade =  18
+endereco = 'Rua 70'
+print('Nome', nome )
+print('idade', idade)
+print('endereço', endereco)
+
